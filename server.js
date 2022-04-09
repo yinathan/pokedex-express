07f90ccfcea880602ca4a8c9710fc2fa544571ac
Nathan Yi <nathanyi112@gmail.com>
2022-04-09 17:32:49 -0500

    adds pokemon object to /pokemon

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require("dotenv").config()
+
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 const pokemon = require('./models/pokemon.js');
 
-
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon)
+})
 
 
 app.listen(PORT, () => {
