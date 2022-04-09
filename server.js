b85806a1be3c1add306019356b26bb00d426fb58
Nathan Yi <nathanyi112@gmail.com>
2022-04-09 17:34:56 -0500

    adds restful routes pseudocode

--- server.js
+++ server.js
@@ -5,9 +5,22 @@ const app = express()
 const PORT = process.env.PORT || 3001;
 const pokemon = require('./models/pokemon.js');
 
+// Index   GET /pokemon
 app.get("/pokemon", (req, res) => {
     res.send(pokemon)
 })
+
+// Show  GET /pokemon/:id
+
+// New GET /pokemon/new
+
+//Edit GET /pokemon/:id/edit
+
+// Create POST /pokemon
+
+// Update  PUT  /pokemon/:id
+
+// Destroy  Delete   /pokemon/:id
 
 
 app.listen(PORT, () => {
