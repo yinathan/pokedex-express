00358398f69848b3dcb7b3edc67288e711b42646
Nathan Yi <nathanyi112@gmail.com>
2022-04-15 18:07:07 -0500

    adds new/post/delete routes

--- server.js
+++ server.js
@@ -18,23 +18,33 @@ app.get("/pokemon", (req, res) => {
     res.render("index.ejs", { pokedex: pokemon })
 })
 
-// Show  GET /pokemon/:id
-app.get("/pokemon/:id", (req, res) => {
-    
-    res.render("show.ejs", { pokeIndex: pokemon[req.params.id] })
-    
-})
+
 
 // New GET /pokemon/new
-
+app.get("/pokemon/new", (req, res) => {
+    res.render("new.ejs", {pokemon: pokemon})
+})
 //Edit GET /pokemon/:id/edit
-
+app.get("/pokemon/:id/edit", (req, res) => {
+    res.send("working")
+})
 // Create POST /pokemon
-
+app.post('/pokemon', (req, res) => {
+    pokemon.push(req.body)
+    res.redirect("/pokemon")
+})
 // Update  PUT  /pokemon/:id
 
 // Destroy  Delete   /pokemon/:id
-
+app.delete("/pokemon/:id", (req, res) => {
+  const index = req.params.id;
+  pokemon.splice(index, 1);
+  res.redirect("/pokemon");
+});
+// Show  GET /pokemon/:id
+app.get("/pokemon/:id", (req, res) => {
+    res.render("show.ejs", { pokeIndex: pokemon[req.params.id] })
+})
 
 app.listen(PORT, () => {
     console.log("working")
